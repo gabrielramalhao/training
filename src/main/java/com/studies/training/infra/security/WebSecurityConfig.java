63222a7b790b3f4fed8e35f683ffd2d1b6114437
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-05-18 12:52:05 -0300

    feat: added username and password

--- src/main/java/com/studies/training/infra/security/WebSecurityConfig.java
+++ src/main/java/com/studies/training/infra/security/WebSecurityConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
@@ -15,5 +21,17 @@ public class WebSecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        @SuppressWarnings("deprecation")
+        UserDetails userdetails = User.withDefaultPasswordEncoder()
+                .username("gab")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userdetails);
     }
 }
